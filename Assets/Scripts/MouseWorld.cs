35359ad0c4d56653c59d3df8808c2c77374d1b2a
Daniel Ting <harmeyforever5435@gmail.com>
2023-06-29 23:23:45 -0400

    Added PostProcess, mouse raycast, and anim

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    private static MouseWorld instance;
+
+    [SerializeField] private LayerMask mousePlaneLayerMask;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    private void Update() {
+        transform.position = MouseWorld.GetPosition();
+    }
+
+    public static Vector3 GetPosition() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, instance.mousePlaneLayerMask);
+        return hitInfo.point;
+    }
+}
